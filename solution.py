44eafe04e02a54edbfaf63044b76138123e47322
svinoczar <alexsupper75@gmail.com>
2024-03-20 01:04:27 +0300

    minor fixes

--- solution.py
+++ solution.py
@@ -18,8 +18,8 @@ def half_division(a, b):
         return half_division(a, b)
     xm = ( a + b ) / 2
     ym = f(xm)
-    # print(f"half division: f(x) = {ym}")
     return xm, ym
+    
     
 def golden_ratio(a, b):
     while (b - a) >= epsilon: 
@@ -34,6 +34,7 @@ def golden_ratio(a, b):
     y = f(x)
     return x, y
     
+    
 def chords(a, b):
     while True:
         x = a - (f_diff(a) * (a - b)) / (f_diff(a) - f_diff(b))
@@ -43,32 +44,6 @@ def chords(a, b):
             return x, f(x)
         elif y > 0: b = x
         else: a = x
-    # iter_count = 0
-    # while iter_count < max_iter:
-    #     f_diff_a = f_diff(a) if diff_a == None else diff_a
-    #     f_diff_b = f_diff(b) if diff_b == None else diff_b
-    #     x_tilda = a - (f_diff_a)/(f_diff_b)*(a - b)
-
-    #     # Добавляем проверку, чтобы x_tilda оставался в пределах диапазона [a, b]
-    #     x_tilda = min(max(x_tilda, a), b)
-
-    #     f_diff_x_tilda = f(x_tilda)
-    #     if abs(f_diff_x_tilda) <= epsilon:
-    #         x_asterisk = x_tilda
-    #         f_asterisk = f(x_tilda)
-    #         return x_asterisk, f_asterisk
-    #     elif f_diff_x_tilda > 0:
-    #         b = x_tilda
-    #         f_diff_b = f_diff_x_tilda
-    #     else:
-    #         a = x_tilda
-    #         f_diff_a = f_diff_x_tilda
-
-    #     iter_count += 1
-
-    # print(f"Warning: chords method did not converge after {max_iter} iterations.")
-    # return None, None
-
 
     
 def newtone(a, b, X = None):
@@ -79,7 +54,11 @@ def newtone(a, b, X = None):
     else:
         return x, f(x)
     
-print(half_division(a, b))
-print(golden_ratio(a, b))
-print(chords(a, b))
-print(newtone(a, b))
+def main():
+    print(half_division(a, b))
+    print(golden_ratio(a, b))
+    print(chords(a, b))
+    print(newtone(a, b))
+    
+if __name__ == '__main__':
+    main()
